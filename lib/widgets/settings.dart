f3c24e7a2989584eefcd1d80e79bb0a27fea4fd5
rishavmngo <rishavinmngo@gmail.com>
2024-09-05 09:39:06 +0530

    designed focused time widget in settings

--- lib/widgets/settings.dart
+++ lib/widgets/settings.dart
@@ -1,35 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
+import 'package:timer_app/widgets/FocusedTime.dart';
 
-class Settings extends StatelessWidget {
-  Settings({super.key});
-  final List<String> entries = <String>["Abc", "cdf", "lfg"];
+class Settings extends StatefulWidget {
+  const Settings({super.key});
 
   @override
+  State<Settings> createState() => _SettingsState();
+}
+
+class _SettingsState extends State<Settings> {
+  int _selectedItem = 60;
+  @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Text("Focused time",
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-        ),
-        SizedBox(
-          height: 50, // Specify a fixed height for the ListView
-          child: ListView.builder(
-            scrollDirection: Axis.horizontal,
-            itemCount: entries.length,
-            itemBuilder: (context, index) {
-              return Container(
-                margin: EdgeInsets.symmetric(horizontal: 4),
-                padding: EdgeInsets.all(8),
-                color: Colors.red,
-                child: Center(child: Text(entries[index])),
-              );
-            },
-          ),
-        ),
-      ],
+    return Container(
+      decoration: BoxDecoration(color: Colors.grey.shade300),
+      child: Column(
+        children: [FocusedTime()],
+      ),
     );
   }
 }
