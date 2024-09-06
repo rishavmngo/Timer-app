400dac17bdecb10075876360fb188b811bb18117
rishavmngo <rishavinmngo@gmail.com>
2024-09-06 11:14:41 +0530

    completed ui of login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:timer_app/firebase_options.dart';
 import 'package:timer_app/pages/home_page.dart';
+import 'package:timer_app/pages/login_page.dart';
 import 'package:timer_app/theme.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const ProviderScope(child: MyApp()));
 }
 
@@ -15,6 +20,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         theme: lightTheme,
         debugShowCheckedModeBanner: false,
-        home: const HomePage());
+        //home: const HomePage());
+        home: const LoginPage());
   }
 }
